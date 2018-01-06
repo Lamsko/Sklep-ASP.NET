cf50e4872ddd88a7936c53f94d6b7ed235a6148a
Witold Mormul <w.mormul@gmail.com>
2018-01-06 13:34:56 +0100

    ProductDetails.cs GetProducts

--- Sklep/ProductDetails.aspx.cs
+++ Sklep/ProductDetails.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Sklep.Models;
+using System.Web.ModelBinding;
 
 namespace Sklep
 {
@@ -12,6 +14,21 @@ namespace Sklep
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public IQueryable<Product> GetProduct([QueryString("productID")] int?productId)
+        {
+            var _db = new Sklep.Models.ProductContext();
+            IQueryable<Product> query = _db.Products;
+            if (productId.HasValue && productId > 0)
+            {
+                query = query.Where(p => p.ProductID == productId);
+            }
+            else
+            {
+                query = null;
+            }
+            return query;
         }
     }
 }
