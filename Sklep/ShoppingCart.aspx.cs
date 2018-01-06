136b2257de680e9468b177245b2ecee011379e06
Witold Mormul <w.mormul@gmail.com>
2018-01-06 16:58:47 +0100

    ShoppingCart total

--- Sklep/ShoppingCart.aspx.cs
+++ Sklep/ShoppingCart.aspx.cs
@@ -13,7 +13,21 @@ namespace Sklep
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            using (ShoppingCartActions usersShoppingCart = new ShoppingCartActions())
+            {
+                decimal cartTotal = 0;
+                cartTotal = usersShoppingCart.GetTotal();
+                if (cartTotal > 0)
+                {
+                    lblTotal.Text = String.Format("{0:c}", cartTotal);
+                }
+                else
+                {
+                    LabelTotalText.Text = "";
+                    lblTotal.Text = "";
+                    ShoppingCartTitle.InnerText = "Koszyk jest pusty";
+                }
+            }
         }
 
         public List<CartItem> GetShoppingCartItems()
