290df5e93dee4e421f7611cee1075e82ebec18f3
Witold Mormul <w.mormul@gmail.com>
2018-01-06 15:09:40 +0100

    ShoppingCartActions.cs

--- Sklep/Models/ProductContext.cs
+++ Sklep/Models/ProductContext.cs
@@ -10,5 +10,6 @@ namespace Sklep.Models
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<CartItem> ShoppingCartItems { get; set; }
     }
 }
