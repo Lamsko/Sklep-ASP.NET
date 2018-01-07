5946584b8de0d48bab7c953373183a7033810993
Witold Mormul <w.mormul@gmail.com>
2018-01-07 13:17:31 +0100

    Site.master cart

--- Sklep/ShoppingCart.aspx.cs
+++ Sklep/ShoppingCart.aspx.cs
@@ -1,14 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
 using System.Web.UI.WebControls;
 using Sklep.Models;
 using Sklep.Logic;
-using System.Collections;
 using System.Collections.Specialized;
-using System.Web.ModelBinding;
 
 namespace Sklep
 {
