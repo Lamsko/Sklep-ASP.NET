530b77b9187bfce6432aedbee74b7e8a7c7205e4
Witold Mormul <w.mormul@gmail.com>
2018-01-10 10:07:51 +0100

    Checkout sites

--- Sklep/Models/ProductContext.cs
+++ Sklep/Models/ProductContext.cs
@@ -2,14 +2,16 @@
 
 namespace Sklep.Models
 {
-    public class ProductContext : DbContext
-    {
-        public ProductContext() : base("Sklep")
-        {
+	public class ProductContext : DbContext
+	{
+		public ProductContext() : base("Sklep")
+		{
 
-        }
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Product> Products { get; set; }
-        public DbSet<CartItem> ShoppingCartItems { get; set; }
-    }
+		}
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Product> Products { get; set; }
+		public DbSet<CartItem> ShoppingCartItems { get; set; }
+		public DbSet<Order> Orders { get; set; }
+		public DbSet<OrderDetail> OrderDetails { get; set; }
+	}
 }
