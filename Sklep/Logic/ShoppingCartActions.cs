78eec6d3987001b906df754e890d30474991f829
Witold Mormul <w.mormul@gmail.com>
2018-01-10 11:52:01 +0100

    PayPal integration

--- Sklep/Logic/ShoppingCartActions.cs
+++ Sklep/Logic/ShoppingCartActions.cs
@@ -206,5 +206,16 @@ namespace Sklep.Logic
 			public int PurchaseQuantity;
 			public bool RemoveItem;
 		}
+
+		public void MigrateCart(string cartId, string userName)
+		{
+			var shoppingCart = _db.ShoppingCartItems.Where(c => c.CartId == cartId);
+			foreach(CartItem item in shoppingCart)
+			{
+				item.CartId = userName;
+			}
+			HttpContext.Current.Session[CartSessionKey] = userName;
+			_db.SaveChanges();
+		}
 	}
 }
